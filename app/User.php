155f3dbacbe5570c36a83f5999aa2737c9a1191b
M511-18\admin <s3A432058@student.ncut.edu.tw>
2017-10-19 16:47:26 +0800

    陳欣妤-在 User 模型中定義一個  tasks 函式，並呼叫 Eloquent 提供的 hasMany 方法

--- app/User.php
+++ app/User.php
@@ -7,23 +7,36 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+
+   /* use Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array
-     */
+
     protected $fillable = [
         'name', 'email', 'password',
     ];
-
+*/
     /**
      * The attributes that should be hidden for arrays.
      *
      * @var array
      */
+    /*
     protected $hidden = [
         'password', 'remember_token',
     ];
+    */
+
+    // 其他的 Eloquent 屬性...
+
+    /**
+     * 取得該使用者的所有任務。
+     */
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
