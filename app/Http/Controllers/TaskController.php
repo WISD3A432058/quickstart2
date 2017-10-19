5634d559c82887373c04a8611125826459647747
M511-18\admin <s3A432058@student.ncut.edu.tw>
2017-10-19 18:13:23 +0800

    陳欣妤-在 TaskController 控制器的建構子中使用「型別提示」，並在  index 路由中使用

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,20 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Task;
 use App\Http\Requests;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Repositories\TaskRepository;
 
 class TaskController extends Controller
 {
     /**
-     * 建立一個新的控制器實例。
+     * 任務資源庫的實例。
      *
+     * @var TaskRepository
+     */
+    protected $tasks;
+
+    /**
+     * 建立新的控制器實例。
+     *
+     * @param  TaskRepository  $tasks
      * @return void
      */
-    public function __construct()
+    public function __construct(TaskRepository $tasks)
     {
         $this->middleware('auth');
+
+        $this->tasks = $tasks;
     }
     /**
      * 顯示使用者所有任務的清單。
@@ -28,7 +40,7 @@ class TaskController extends Controller
         $tasks = Task::where('user_id', $request->user()->id)->get();
 
         return view('tasks.index', [
-            'tasks' => $tasks,
+            'tasks' => $this->tasks->forUser($request->user()),
         ]);
     }
 
