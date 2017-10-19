2b4ef6b15b57f5520cef2ce9f8ccf42faf7ec8f4
M511-18\admin <s3A432058@student.ncut.edu.tw>
2017-10-19 18:13:45 +0800

    陳欣妤-將任務切分並將它們顯示至表格中

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -6,7 +6,7 @@
  * Time: 下午 05:28
  */
 // resources/views/tasks/index.blade.php
-
+/*
 @extends('layouts.app')
 
 @section('content')
@@ -42,4 +42,45 @@
     </div>
 
     <!-- 代辦：目前任務 -->
+*/
+@extends('layouts.app')
+
+@section('content')
+    <!-- 建立任務表單... -->
+
+    <!-- 目前任務 -->
+    @if (count($tasks) > 0)
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                目前任務
+            </div>
+
+            <div class="panel-body">
+                <table class="table table-striped task-table">
+
+                    <!-- 表頭 -->
+                    <thead>
+                    <th>Task</th>
+                    <th>&nbsp;</th>
+                    </thead>
+
+                    <!-- 表身 -->
+                    <tbody>
+                    @foreach ($tasks as $task)
+                        <tr>
+                            <!-- 任務名稱 -->
+                            <td class="table-text">
+                                <div>{{ $task->name }}</div>
+                            </td>
+
+                            <td>
+                                <!-- 代辦：刪除按鈕 -->
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    @endif
 @endsection
